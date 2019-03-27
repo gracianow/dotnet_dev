2dc5cb2449d2b02ee0d36e6c80b5cf1cfefbeda3
gracianow <gracianow@msn.com>
2019-03-27 20:07:16 -0300

    Atualizando o repositorio

---
+++ MvcLivroRepo/MvcLivroRepo/Models/ILivroRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcLivroRepo.Models
+{
+    interface ILivroRepositorio : IDisposable
+    {
+        IEnumerable<Livro> GetLivros();
+        Livro GetLivroPorID(int livroId);
+        void InserirLivro(Livro livro);
+        void DeletarLivro(int bookID);
+        void AtualizaLivro(Livro livro);
+        void Salvar();
+    }
+}
---
+++ MvcLivroRepo/MvcLivroRepo/Models/LivroContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcLivroRepo.Models
+{
+    public class LivroContext : DbContext
+    {
+        public LivroContext()
+            : base("name=LivrariaDbContext")
+        {
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
